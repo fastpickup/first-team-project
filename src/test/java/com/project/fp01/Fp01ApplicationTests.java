d264be36d226ecc08c69d9a9a94a4bd54bfe2e74
jo <whtkdgml36271@gmail.com>
2023-07-26 10:06:03 +0900

    setting

--- src/test/java/com/project/fp01/Fp01ApplicationTests.java
+++ src/test/java/com/project/fp01/Fp01ApplicationTests.java
@@ -3,11 +3,21 @@ package com.project.fp01;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @SpringBootTest
 class Fp01ApplicationTests {
 
+	private DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	public void connetionTest() {
+		try(Connection connection  = dataSource.getConnection()) {
+
+		} catch (Exception e) {
+
+		}
 	}
 
 }
