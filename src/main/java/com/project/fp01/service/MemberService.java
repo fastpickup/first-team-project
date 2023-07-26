8dc537bf38de20250426daef1ac9b6e518b94015
jo <whtkdgml36271@gmail.com>
2023-07-26 10:46:16 +0900

    service commit

--- src/main/java/com/project/fp01/service/MemberService.java
+++ src/main/java/com/project/fp01/service/MemberService.java
@@ -1,5 +1,24 @@
 package com.project.fp01.service;
 
-public class MemberService {
-    
+import com.project.fp01.dto.MemberDTO;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface MemberService {
+  // join Member
+  int joinMember(MemberDTO memberDTO);
+
+  // read Member
+  MemberDTO readMember(String eamil);
+
+  // delete Mmeber
+  int deleteMember(String email);
+
+  // update Member
+  int updateMember(MemberDTO memberDTO);
+
+  // list Member
+  List<MemberDTO> listMember();
 }
--- src/main/java/com/project/fp01/service/impl/MemberServiceImpl.java
+++ src/main/java/com/project/fp01/service/impl/MemberServiceImpl.java
@@ -1,5 +1,61 @@
 package com.project.fp01.service.impl;
 
-public class MemberServiceImpl {
-    
+import com.project.fp01.dto.MemberDTO;
+import com.project.fp01.mappers.MemberMapper;
+import com.project.fp01.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+  //의존성 주입
+  private final MemberMapper memberMapper;
+
+  //join Member
+  @Override
+  public int joinMember(MemberDTO memberDTO) {
+    log.info("join Service------------------------------------------------");
+
+    return memberMapper.joinMember(memberDTO);
+  }
+
+  //read Member
+  @Override
+  public MemberDTO readMember(String eamil) {
+    log.info("read Service------------------------------------------------");
+
+    return memberMapper.readMember(eamil);
+  }
+
+  //delete Member
+  @Override
+  public int deleteMember(String email) {
+    log.info("delete Service------------------------------------------------");
+
+    return memberMapper.deleteMember(email);
+  }
+
+  //update Member
+  @Override
+  public int updateMember(MemberDTO memberDTO) {
+    log.info("update Service------------------------------------------------");
+
+    return memberMapper.updateMember(memberDTO);
+  }
+
+  //list Member
+  @Override
+  public List<MemberDTO> listMember() {
+    log.info("list Service------------------------------------------------");
+
+    return memberMapper.listMember();
+  }
+
+  //
 }
