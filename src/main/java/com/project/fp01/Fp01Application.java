6632964f42185d7bc9523ef76baf0d7567134902
JY LEE <wndyd0110@gmail.com>
2023-07-26 10:19:24 +0900

    dsf

--- src/main/java/com/project/fp01/Fp01Application.java
+++ src/main/java/com/project/fp01/Fp01Application.java
@@ -9,6 +9,7 @@ public class Fp01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Fp01Application.class, args);
 		System.out.println("이주용 비잉신");
+		System.out.println("권성준 비잉신");
 	}
 
 }
