d03f8fa29239a0cc8c016538d4e1c19d1b2f8595
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-07-26 10:27:25 +0900

    message commit dto, mapper

--- src/main/java/com/project/fp01/dto/MemberDTO.java
+++ src/main/java/com/project/fp01/dto/MemberDTO.java
@@ -1,5 +1,21 @@
 package com.project.fp01.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDTO {
-    
+    private String email;
+    private String memberPw;
+    private String memberName;
+    private String memberPhone;
 }
--- src/main/java/com/project/fp01/mappers/MemberMapper.java
+++ src/main/java/com/project/fp01/mappers/MemberMapper.java
@@ -1,5 +1,27 @@
 package com.project.fp01.mappers;
 
-public class MemberMapper {
-    
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.project.fp01.dto.MemberDTO;
+
+@Mapper
+public interface MemberMapper {
+
+    // join member
+    int joinMember(MemberDTO memberDTO);
+
+    // read Member
+    MemberDTO readMember(String eamil);
+
+    // delete Mmeber
+    int deleteMember(String email);
+
+    // update Member
+    int updateMember(MemberDTO memberDTO);
+
+    // list Member
+    List<MemberDTO> listMember();
+
 }
