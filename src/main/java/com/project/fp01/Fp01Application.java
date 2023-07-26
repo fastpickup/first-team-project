d0e5b83fed700d568c38c7b9be136af6164c4525
jo <whtkdgml36271@gmail.com>
2023-07-26 09:39:16 +0900

    0726 project setting

---
+++ src/main/java/com/project/fp01/Fp01Application.java
@@ -0,0 +1,13 @@
+package com.project.fp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fp01/Fp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
